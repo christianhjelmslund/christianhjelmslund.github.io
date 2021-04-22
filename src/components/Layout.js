ec13e5a13a363b58bad7732d4f12a7dad59d9bc0
Christian <christianhjelmslund@gmail.com>
2021-04-22 20:00:48 +0200

    Removed the decoding of buttons as part of post componenet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,11 @@
 import React from "react";
 import NavigationBar from "./UI/NavigationBar/NavigationBar";
 import styles from "../styles/components/Layout.module.css"
-import { useCookies } from "react-cookie"
 
 const Layout = props => {
-    const [cookies] = useCookies(['cookie-name']);
-    let background = styles.background
-    if (cookies['alternative-background']) {
-        background = styles.alternativeBackground
-    }
     return (
         <React.Fragment >
-            <div className={background}/>
+            <div className={styles.background}/>
             <NavigationBar/>
             <main>{props.children}</main>
         </React.Fragment>
--- src/hooks/decodePost.js
+++ src/hooks/decodePost.js
@@ -1,12 +1,9 @@
-import React, {useState} from "react";
+import React from "react";
 import {Image} from "react-bootstrap";
 import styles from "../styles/pages/PostPage.module.css";
-import StyledButton from "../components/UI/StyledButton";
-import { useCookies } from "react-cookie"
 
 export default post => {
     if (!post) { return post }
-    const [, setCookie, deleteCookie] = useCookies();
     const components = []
     if (post.content) {
         for (let paragraph in post.content) {
@@ -23,34 +20,6 @@ export default post => {
             img = post.images[img]
             components[img.idx] = <div key={img.idx} className={styles.divImg}>
                 <Image className={"img-fluid"} src={img.url}/>
-            </div>
-        }
-    }
-    if (post.buttons) {
-        for (let btn in post.buttons) {
-            btn = post.buttons[btn]
-            let btnFunction
-            if (btn.function) {
-                if (btn.function.cookie) {
-                    const mode = Object.keys(btn.function.cookie)[0]
-                    const key = Object.keys(btn.function.cookie[mode])[0]
-                    const val = btn.function.cookie[mode][key]
-                    if (mode === "set") {
-                        btnFunction = () => {
-                            setCookie(key,val)
-                        }
-                    } else {
-                        btnFunction = () => {
-                            deleteCookie(key,val)
-                        }
-                    }
-                }
-            }
-            components[btn.idx] = <div key={btn.idx} className={styles.divImg}>
-                <StyledButton buttonTitle={btn.title}
-                              variant={btn.variant}
-                              clicked={btnFunction}>
-                </StyledButton>
             </div>
         }
     }
