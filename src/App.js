3ec777a8a6acc4d5c7e5ac4c72d59cf361817cd5
Christian Hjelmslund <christianhjelmslund@gmail.com>
2021-07-28 16:01:09 +0200

    Use lazy loading for all paths (#79)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Switch, Redirect, HashRouter} from "react-router-dom";
 
 import './styles.scss'
@@ -6,31 +6,34 @@ import ScrollToTop from "./hooks/scrollToTop";
 
 import Layout from "./components/Layout";
 import Posts from "./pages/Home"
-import PostPage from "./pages/Post"
-import About from "./pages/About"
-import CookiePage from "./pages/CookieConsent"
-import Contact from "./pages/Contact";
-import Services from "./pages/Services";
+import Spinner from "./components/UI/Spinner"
+const Post = lazy(() => import('./pages/Post'));
+const Services = lazy(() => import('./pages/Services'));
+const Contact = lazy(() => import('./pages/Contact'));
+const About = lazy(() => import('./pages/About'));
+const CookieConsent = lazy(() => import('./pages/CookieConsent'));
 
 const App = () => {
     let routes = (
         <>
             <ScrollToTop />
             <Switch>
+                <Suspense fallback={<Spinner/>}>
                 <Route path={"/"} exact component={Posts}/>
                 <Route path={"/about"} component={About}/>
                 <Route path={"/contact"} component={Contact}/>
                 <Route path={"/services"} component={Services}/>
-                <Route path={"/cookie_consent"} component={CookiePage}/>
-                <Route path={"/:postId"} component={PostPage}/>
+                <Route path={"/cookie_consent"} component={CookieConsent}/>
+                <Route path={"/:postId"} component={Post}/>
                 <Redirect to={"/"}/>
+                </Suspense>
             </Switch>
         </>
     )
 
     return (
         <HashRouter>
-            <Layout >
+            <Layout>
                 {routes}
             </Layout>
         </HashRouter>
